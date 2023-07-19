e57b62e20761aca52368d4aef8fba4148ddec4cf
stoopidian <yutung_chen@mymail.sutd.edu.sg>
2023-07-19 15:36:17 +0800

    piechart + tickets(inprogress)

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/headers/NavBar' 
 import axios from "axios" 
 import "../components/headers/assets/css/style.css" 
+import Pie from '../components/Pie'
+import DesTable from '../components/ST_Des_summary'
 
 
 function Home() { 
@@ -25,10 +27,75 @@ function Home() {
             <div class="hero__left"> 
               <section class="home-collection">
                 <div class="home-content">
-                  <span class="home-caption">DASHBOARD</span>
+                  <Pie/>
                 </div>
-                <div class="home-main">
-                  <div class="home-card">
+
+                <div class="home-container4">
+
+                  {/* <div class="home-container5">
+                    <span class="home-text08">Description</span>
+                  </div> */}
+{/* 
+                  <div class="home-container7"><span class="home-text10">Date</span></div> */}
+
+                </div>
+              </section>
+              
+            </div> 
+            <div class ="hero_right">
+              <section class="home-collection-right">
+              <div class="home-main">
+   
+                <div class="home-container3">
+
+                  <div class="item">
+                  <article class="ticket">
+  <header class="ticket__wrapper">
+    <div class="ticket__header">
+      2 🎟
+    </div>
+  </header>
+  <div class="ticket__divider">
+    <div class="ticket__notch"></div>
+    <div class="ticket__notch ticket__notch--right"></div>
+  </div>
+  <div class="ticket__body">
+    <section class="ticket__section">
+      <h3>Description</h3>
+      <p>Level 1 Aircon</p>
+    </section>
+  </div>
+    <div class="ticket__body">
+    <section class="ticket__section">
+      <h3>Date</h3>
+      <p>20 Oct 2023</p>
+    </section>
+  </div>
+  <footer class="ticket__footer">
+    <span>Comments</span>
+
+  </footer>
+</article>
+                    {/* <DesTable/> */}
+                  </div>
+
+
+
+
+              </div> 
+              </div>
+                
+              </section>
+            </div>
+        </div> 
+  </div>
+)} 
+export default Home
+
+//change
+
+                  // {/* /* <span class="home-caption">DASHBOARD</span> */ */}
+                                    {/* <div class="home-card">
                     <div class="home-image2">
                       <img
                         alt="image"
@@ -57,41 +124,14 @@ function Home() {
                     <div class="home-content3">
                       <span class="home-caption3">Ongoing: 05</span>
                     </div>
-                  </div>
-                  <div class="home-container3">
-                  <div class="home-container9">
-
-                  </div>
-                </div>
-
-
-                </div>
-                <div class="home-container4">
-                  <div class = "wrapper">
-                    <div>Description</div>
-                    <div>Date</div>
-                    <div>Status</div>
-                  </div>
-                  {/* <div class="home-container5">
-                    <span class="home-text08">Description</span>
                   </div> */}
-{/* 
-                  <div class="home-container7"><span class="home-text10">Date</span></div> */}
-                  <div class="home-container8">
-                    <input type="checkbox" checked="true" class="home-checkbox" />
-                    <input type="checkbox" checked="true" class="home-checkbox1" />
-                    <input type="checkbox" checked="true" class="home-checkbox2" />
-                    <input type="checkbox" checked="true" class="home-checkbox3" />
-                    <input type="checkbox" checked="true" class="home-checkbox4" />
-                    <input type="checkbox" checked="true" class="home-checkbox5" />
-                  </div>
-                </div>
-              </section>
-            </div> 
-        </div> 
-  </div>
-)} 
-export default Home
 
-//change
 
+                //   <div class="home-container8">
+                //   <input type="checkbox" checked="true" class="home-checkbox" />
+                //   <input type="checkbox" checked="true" class="home-checkbox1" />
+                //   <input type="checkbox" checked="true" class="home-checkbox2" />
+                //   <input type="checkbox" checked="true" class="home-checkbox3" />
+                //   <input type="checkbox" checked="true" class="home-checkbox4" />
+                //   <input type="checkbox" checked="true" class="home-checkbox5" />
+                // </div>
