e12dad5a40ddb367658d4524c8978ea7c254dbf5
NithyaRV <127846590+NithyaRV@users.noreply.github.com>
2023-08-01 18:24:57 +0800

    Error and success box update

--- src/components/tenantComponents/STTable.js
+++ src/components/tenantComponents/STTable.js
@@ -12,6 +12,8 @@ import "swiper/css"
 import 'swiper/css/scrollbar';
 import {Scrollbar,Mousewheel} from "swiper/modules"
 import Ticket from ".././STTicketsNew"
+import { useError } from "../components/errorBox";
+import { useSuccess } from "../components/successBox";
 
 
 const ServiceTicketHistoryTable = (props) => {
@@ -19,6 +21,8 @@ const ServiceTicketHistoryTable = (props) => {
   const [infoTicketOpen, setInfoTicketOpen] = useState(false);
   const [selectedTicket, setSelectedTicket] = useState (false);
   const [data,setData] = useState([])
+  const { showError } = useError();
+  const { showSuccess } = useSuccess();
 
     const handleAddTicket = () => {
       setAddTicketOpen(true)
@@ -31,6 +35,7 @@ const ServiceTicketHistoryTable = (props) => {
         setData(data);
       } catch (error) {
         console.error('Error fetching data:', error); /**TODO: Display an error on the UI instead */
+        showError(('Error fetching data:', error), 3000);
       }
     };
     const handleDeleteTicket = async(rowData) => {
@@ -41,6 +46,7 @@ const ServiceTicketHistoryTable = (props) => {
         setData([...data.splice(0,idx),...data.splice(idx,data.length)])
       } else {
         console.log(data) /** TODO: Add UI Error component */
+        showError(data, 3000);
       }
     }
 
